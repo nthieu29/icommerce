6400694e771b996444b704bceb33b07d0694d211
Ngo Trung Hieu <hieu.ngo@axonactive.com>
2020-07-07 13:27:18 +0700

    Update database configuration (to use Postgres)

--- product-service/src/test/java/com/nthieu/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/nthieu/productservice/ProductServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.nthieu.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductServiceApplicationTests {
 
     @Test
