03ef64346b25555610ff09493b70decce670f54e
nthieu29 <ngotrunghieu@live.com>
2020-07-08 20:27:10 +0700

    Add custom TaskExecutor

--- product-service/src/main/java/com/nthieu/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/nthieu/productservice/ProductServiceApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
@@ -20,4 +24,13 @@ public class ProductServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
+
+    @Bean
+    public Executor taskExecutor() {
+        // We need execute many short-lived asynchronous tasks (send customer activities to CloudAMQP)
+        // so Cached Thread Pool already meet our needs
+        // for more advanced, we could use ThreadPoolExecutor
+        return Executors.newCachedThreadPool();
+    }
+
 }
